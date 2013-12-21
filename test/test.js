2372342a19a4ac5661ff3d5c11850e12150825df
Evan Tahler <evan@taskrabbit.com>
2013-12-20 17:39:14 -0800

    travis test

--- test/test.js
+++ test/test.js
@@ -48,7 +48,9 @@ describe("ELASTICDUMP", function(){
   beforeEach(function(done){
     this.timeout = 10 * 1000;
     clear(function(){
+      console.log("CLEAR")
       seed(function(){
+        console.log("SEET")
         setTimeout(function(){
           done();
         }, 500);
