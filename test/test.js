7b79784beb63088362cfc5920dcda013635ae346
ferron <ferronrsmith@gmail.com>
2019-09-21 16:21:58 -0400

    quick update

--- test/test.js
+++ test/test.js
@@ -14,7 +14,7 @@ const baseUrl = 'http://127.0.0.1:9200'
 
 const seeds = {}
 const seedSize = 500
-const testTimeout = seedSize * 100
+const testTimeout = seedSize * 200
 let i = 0
 let indexesExistingBeforeSuite = 0
 
