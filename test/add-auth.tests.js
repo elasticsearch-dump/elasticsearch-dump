c22c34b239824edc06bca4788d732efdf6313f61
ferron <ferronrsmith@gmail.com>
2020-07-08 12:58:03 -0400

    Revert "added backward compat patch for URL not being in global scope in node v8"
    This reverts commit 13237f021dbda2a1d800b3abbd1e0b7b24d6c452.

--- test/add-auth.tests.js
+++ test/add-auth.tests.js
@@ -1,22 +1,21 @@
-require('../lib/patch')
-const addAuth = require('../lib/add-auth')
-const should = require('should'); // eslint-disable-line
+var addAuth = require('../lib/add-auth')
+var should = require('should') // eslint-disable-line
 
-describe('add-auth', () => {
-  it('should throw ENOENT if the auth file is missing', () => {
-    (() => {
+describe('add-auth', function () {
+  it('should throw ENOENT if the auth file is missing', function () {
+    (function () {
       addAuth('http://blah.com', 'NotAFile.txt')
     }).should.throw('ENOENT: no such file or directory, open \'NotAFile.txt\'')
   })
-  it('should throw error if the auth file is missing username and password', () => {
-    (() => {
+  it('should throw error if the auth file is missing username and password', function () {
+    (function () {
       addAuth('http://blah.com', 'test/test-resources/malformedHttpAuth.ini')
     }).should.throw('Malformed Auth File')
   })
-  it('shouldn\'t overwrite existing auth parameters in url', () => {
+  it('shouldn\'t overwrite existing auth parameters in url', function () {
     addAuth('http://user:pass@blah.com', 'test/test-resources/httpAuthTest.ini').should.equal('http://user:pass@blah.com')
   })
-  it('should add auth parameters if they are missing', () => {
+  it('should add auth parameters if they are missing', function () {
     addAuth('http://blah.com', 'test/test-resources/httpAuthTest.ini').should.equal('http://foo:bar@blah.com/')
   })
 })
