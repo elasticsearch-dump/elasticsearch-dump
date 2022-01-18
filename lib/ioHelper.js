elasticsearch-dump/elasticsearch-dump

commit 4d09f90c6067108f5b77fe31b4463e09ff33db8b

fix dump of indexes with %

lib/ioHelper.js
@@ -1,4 +1,4 @@
-const { isUrl, isCsvUrl } = require('./is-url')
+const { isUrl, isCsvUrl, encodeURIOnce } = require('./is-url')
 const addAuth = require('./add-auth')
 const path = require('path')
 const s3urls = require('s3urls')
@@ -11,7 +11,7 @@ const getIo = (elasticdump, type) => {
       if (elasticdump.options.httpAuthFile) {
         elasticdump.options[type] = addAuth(elasticdump.options[type], elasticdump.options.httpAuthFile)
       }
-    } else if (s3urls.valid(elasticdump.options[type])) {
+    } else if (s3urls.valid(encodeURIOnce(elasticdump.options[type]))) {
       elasticdump[`${type}Type`] = 's3'
     } else if (isCsvUrl(elasticdump.options[type])) {
       elasticdump[`${type}Type`] = 'csv'

lib/is-url.js
@@ -6,7 +6,7 @@ const csvUrlRegex = /^(csv:\/\/)(.+)$/
 // should not start with http:// or https:// and urls should.
 const isUrl = (url) => {
   if (!url) return false
-  url = url.toString()
+  url = decodeURIComponent(encodeURIOnce(url.toString()))
   return url.indexOf('http://') === 0 || url.indexOf('https://') === 0
 }
 
@@ -32,8 +32,25 @@ const isCsvUrl = (filePath) => {
   return !!matches
 }
 
+const encodeURIOnce = (uri) => {
+  let isEncoded
+
+  try {
+    isEncoded = uri !== decodeURIComponent(uri)
+  } catch (error) {
+    if (error instanceof URIError) {
+      isEncoded = false
+    } else {
+      throw error
+    }
+  }
+
+  return isEncoded ? uri : encodeURIComponent(uri)
+}
+
 module.exports = {
   isUrl,
   isCsvUrl,
-  fromCsvUrl
+  fromCsvUrl,
+  encodeURIOnce
 }

test/is-url.tests.js
@@ -1,5 +1,5 @@
-const { isUrl } = require('../lib/is-url')
-var should = require('should') // eslint-disable-line
+const { isUrl, encodeURIOnce } = require('../lib/is-url')
+require('should')
 
 describe('is-url', () => {
   it('returns true if url starts with http://', () => {
@@ -19,5 +19,26 @@ describe('is-url', () => {
   })
   it('returns false for unix file path', () => {
     isUrl('/some/unix/path').should.equal(false)
+  })
+  it('returns true if url is encoded', () => {
+    isUrl('http%3A%2F%2Fgithub.com%2Fkuzzleio%2Fkuzzle').should.equal(true)
+  })
+})
+
+describe('encodeURIOnce', () => {
+  const decodedURI = 'http://github.com/kuzzleio/kuzzle'
+  const encodedURI = 'http%3A%2F%2Fgithub.com%2Fkuzzleio%2Fkuzzle'
+
+  it('returns encoded URI if not encoded', () => {
+    encodeURIOnce(decodedURI).should.equal(encodedURI)
+  })
+  it('returns same encoded URI if already encoded', () => {
+    encodeURIOnce(encodedURI).should.equal(encodedURI)
+  })
+  it('returns encoded partial URI component', () => {
+    encodeURIOnce('%kuzzle').should.equal('%25kuzzle')
+  })
+  it('returns same encoded partial URI component if already encoded', () => {
+    encodeURIOnce('%25kuzzle').should.equal('%25kuzzle')
   })
 })

commit 3c57591a8fceec35f846247ab832a0f56e943b6a

:bug: fixed s3 detection logic broken by commit : 4d09f90c6067108f5b77fe31b4463e09ff33db8b
closes #847

lib/ioHelper.js
@@ -1,19 +1,21 @@
-const { isUrl, isCsvUrl, encodeURIOnce } = require('./is-url')
+const { isUrl, isCsvUrl, isS3Prefix } = require('./is-url')
 const addAuth = require('./add-auth')
 const path = require('path')
 const s3urls = require('s3urls')
 
 const getIo = (elasticdump, type) => {
   let EntryProto
-  if (elasticdump.options[type] && !elasticdump.options[`${type}Transport`]) {
-    if (isUrl(elasticdump.options[type])) {
+  const transportPath = elasticdump.options[type]
+  const transport = elasticdump.options[`${type}Transport`]
+  if (transportPath && !transport) {
+    if (isUrl(transportPath)) {
       elasticdump[`${type}Type`] = 'elasticsearch'
       if (elasticdump.options.httpAuthFile) {
-        elasticdump.options[type] = addAuth(elasticdump.options[type], elasticdump.options.httpAuthFile)
+        elasticdump.options[type] = addAuth(transportPath, elasticdump.options.httpAuthFile)
       }
-    } else if (s3urls.valid(encodeURIOnce(elasticdump.options[type]))) {
+    } else if (isS3Prefix && s3urls.valid(transportPath)) {
       elasticdump[`${type}Type`] = 's3'
-    } else if (isCsvUrl(elasticdump.options[type])) {
+    } else if (isCsvUrl(transportPath)) {
       elasticdump[`${type}Type`] = 'csv'
     } else {
       elasticdump[`${type}Type`] = 'file'
@@ -33,12 +35,12 @@ const getIo = (elasticdump, type) => {
     }
 
     EntryProto = require(path.join(__dirname, 'transports', elasticdump[`${type}Type`]))[elasticdump[`${type}Type`]]
-    elasticdump[type] = (new EntryProto(elasticdump, elasticdump.options[type], inputOpts))
-  } else if (elasticdump.options[`${type}Transport`]) {
-    elasticdump[`${type}Type`] = String(elasticdump.options[`${type}Transport`])
-    EntryProto = require(elasticdump.options[`${type}Transport`])
+    elasticdump[type] = (new EntryProto(elasticdump, transportPath, inputOpts))
+  } else if (transport) {
+    elasticdump[`${type}Type`] = String(transport)
+    EntryProto = require(transport) 
     const EntryProtoKeys = Object.keys(EntryProto)
-    elasticdump[type] = (new EntryProto[EntryProtoKeys[0]](elasticdump, elasticdump.options[type], elasticdump.options[`${type}-index`]))
+    elasticdump[type] = (new EntryProto[EntryProtoKeys[0]](elasticdump, transportPath, elasticdump.options[`${type}-index`]))
   }
 }
 

lib/is-url.js
@@ -1,5 +1,6 @@
 const _ = require('lodash')
 const csvUrlRegex = /^(csv:\/\/)(.+)$/
+const s3Regex = /^(s3:\/\/)(.+)$/
 
 // Naive attempt at detecting a url.
 // Should work for elasticdump use case, as file paths
@@ -32,6 +33,12 @@ const isCsvUrl = (filePath) => {
   return !!matches
 }
 
+const isS3Prefix = (filePath) => {
+  s3Regex.lastIndex = 0
+  const matches = s3Regex.exec(filePath)
+  return !!matches
+}
+
 const encodeURIOnce = (uri) => {
   let isEncoded
 
@@ -51,6 +58,7 @@ const encodeURIOnce = (uri) => {
 module.exports = {
   isUrl,
   isCsvUrl,
+  isS3Prefix,
   fromCsvUrl,
   encodeURIOnce
 }