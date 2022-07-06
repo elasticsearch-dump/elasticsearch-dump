4796b463d26f0b01b1fe356d83391a34b41c9932
ferron <fhanse@splunk.com>
2022-07-06 13:19:13 -0400

    quick update

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,10 +1,12 @@
 
 ES_VERSION=${1}
+ES_FLAGS=
 echo "running es-version ${ES_VERSION}"
 if ["${ES_VERSION}" = "7.0.0"]; then
-	ES_DOWNLOAD_URL=https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_VERSION}-linux-x86_64.tar.gz ES_FLAGS=-Ediscovery.type=single-node"
+	ES_DOWNLOAD_URL="https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_VERSION}-linux-x86_64.tar.gz"
+	ES_FLAGS="-Ediscovery.type=single-node"
 else
-	ES_DOWNLOAD_URL=https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-${ES_VERSION}.tar.gz"
+	ES_DOWNLOAD_URL="https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-${ES_VERSION}.tar.gz"
 fi
 
 
@@ -19,3 +21,5 @@ export ES_JAVA_OPTS="-Xms512m -Xmx512m"
 
 # ES needs some time to start
 wget -q --waitretry=1 --retry-connrefused -T 240 -O - http://127.0.0.1:9200
+
+npm test
