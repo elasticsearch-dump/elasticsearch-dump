3e799185a95f695b299e921a285832d3e881c2a0
Alex Kopp <akopp@athenahealth.com>
2025-04-23 06:53:08 -0400

    Support supplying credentials via ELASTICDUMP_USERNAME and ELASTICDUMP_PASSWORD environment variables

--- test/add-auth.tests.js
+++ test/add-auth.tests.js
@@ -2,6 +2,12 @@ const addAuth = require('../lib/add-auth')
 var should = require('should') // eslint-disable-line
 
 describe('add-auth', function () {
+  let envCache
+  before(() => {
+    envCache = process.env
+    process.env.ELASTICDUMP_USERNAME = 'admin'
+    process.env.ELASTICDUMP_PASSWORD = 'password'
+  })
   it('should throw ENOENT if the auth file is missing', function () {
     (function () {
       addAuth('http://blah.com', 'NotAFile.txt')
@@ -14,8 +20,16 @@ describe('add-auth', function () {
   })
   it('shouldn\'t overwrite existing auth parameters in url', function () {
     addAuth('http://user:pass@blah.com', 'test/test-resources/httpAuthTest.ini').should.equal('http://user:pass@blah.com')
+    addAuth('http://user:pass@blah.com', undefined).should.equal('http://user:pass@blah.com')
   })
   it('should add auth parameters if they are missing', function () {
     addAuth('http://blah.com', 'test/test-resources/httpAuthTest.ini').should.equal('http://foo:bar@blah.com/')
+  })
+  it('should use environment variables if they exist', function () {
+    addAuth('http://blah.com', undefined).should.equal('http://admin:password@blah.com/')
+  })
+
+  after(() => {
+    process.env = envCache
   })
 })
